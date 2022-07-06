0253a14df32b890543679c8ae8e757e8a141c791
SKATHEKING <mateusouro1@hotmail.com>
2022-07-06 14:18:39 +0100

    new features

--- starships.py
+++ starships.py
@@ -1,5 +1,6 @@
 import requests
 import pprint
+import pymongo
 
 #does singular api call
 response = requests.get('https://swapi.dev/api/starships/?page=1')
@@ -18,36 +19,48 @@ def api_call_check():
 
 def api_call_all():
 	response = requests.get('https://swapi.dev/api/starships/?page=1')
+	ships_info = []
 	while response.status_code != 404 and response.status_code == 200:
 		try:
 			requests.get(response.json()['next'])
 			response = requests.get(response.json()['next'])
-			pprint.pprint(response.text)
+			pprint.pprint(response.json())
+			for ship in response.json()['results']:
+				ships_info.append(ship)
+
+			return ships_info
 		except:
 			break
 
-#gets pilots from one single page
-def get_pilots_from_single_page():
-	response = requests.get('https://swapi.dev/api/starships/?page=1')
-	pilots = response.json()['results'][4]['pilots']
-
-	for pilot in pilots:
-		do_call(pilot)
-		pprint.pprint(do_call(pilot))
-
-
 #call all pilots apis from within the json api
 def all_pilot_api_call():
-	api_call_all()
-	pilots = response.json()['results'][0]['pilots']
 
-	for pilot in pilots:
-		do_call(pilot)
-		pprint.pprint(do_call(pilot).json())
+	ships = api_call_all()
+	list_of_pilots = []
 
+	try:
+		for pilots in ships:
+			if pilots['pilots'] != [] :
+				for pilot in pilots['pilots']:
+
+					do_call(pilot)
+					list_of_pilots.append(pilot)
+					pprint.pprint(do_call(pilot))
+			else:
+				print('No pilots were found')
+
+	except :
+		print('It was no possible to find any ships')
+
+	finally:
+		return list_of_pilots
+
+
+#inserts into collection
+def insert_into_collection():
+	print('to be done ')
 
 
 #api_call_check()
-#api_call_all()
-get_pilots_from_single_page()
-#all_pilot_api_call()
+#print(api_call_all())
+all_pilot_api_call()
